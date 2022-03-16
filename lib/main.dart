b6aefc79160b4a85a45e6210ba80f7423d8e27cd
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-03-16 20:55:47 +0530

    sizedbox & container added

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Container_Sized.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple,
+      ),
+      home: Container_Sized(),
+    );
+  }
+}
